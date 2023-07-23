3c6af6c50d53998f6ddfdf6a4cafa1d41e34592f
siddhanttyagi <siddhanttyagi432@gmail.com>
2023-07-23 17:42:14 +0530

    first commit

---
+++ flask_backend/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+
+
+@app.route('/posts', methods=['POST'])
+def create_post():
+    try:
+        
+        data = request.get_json()
+        print(data)
+        term = data.get('title')
+        media= data.get('content')
+        entity=data.get('entity')
+        apiurl='https://itunes.apple.com/search'
+        query_params={
+            "term": term,
+            "media": media,
+            "entity": entity
+        }
+        response = requests.get(apiurl, params=query_params)
+        if response.status_code == 200:
+
+            data = response.json() 
+            print(data)
+            list_of_songs=[]
+            for sub_dict in data['results']:
+                list_of_songs.append(sub_dict['collectionName'])
+            return {"data": list_of_songs}
+
+    except Exception as e:
+        # Return an error response if something goes wrong
+        return jsonify({"error": str(e)}), 500
+
+
+
+    
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ frontend_react/src/components/PostApi.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './mystyle.module.css'; 
+
+const MyComponent = () => {
+  // State variables to store form input values
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [entity, setEntity] = useState('');
+  
+  // State variable to hold the response data
+  const [responseData, setResponseData] = useState(null);
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Send a POST request to the server with the form data
+      const response = await axios.post('/posts', {
+        title: title,
+        content: content,
+        entity: entity,
+      });
+
+      // Update the state variable with the response data
+      setResponseData(response.data.data);
+
+      // Reset form fields after successful submission
+      setTitle('');
+      setContent('');
+      setEntity('');
+    } catch (error) {
+      // Handle errors if the request fails (e.g., show an error message)
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Search the given song from here</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Term:</label>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div>
+          <label>Media:</label>
+          <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <div className={styles.idiv2}>
+          <label>Entity:</label>
+          <textarea value={entity} onChange={(e) => setEntity(e.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* Display the response data */}
+      <br></br>
+      {responseData && (
+        <div>
+          <h3>Response Data:</h3>
+          <ul className={styles.list}>
+            {responseData.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyComponent;
